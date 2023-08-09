29c6f09c53af2ede26ad4b7aebccf8e5ec5c69ce
mikeycb96 <michaelsonbaptist@gmail.com>
2023-08-09 17:48:50 +0100

    completed service classes

--- src/main/java/com/bnta/airline/services/FlightService.java
+++ src/main/java/com/bnta/airline/services/FlightService.java
@@ -1,4 +1,31 @@
 package com.bnta.airline.services;
 
+import com.bnta.airline.models.Flight;
+import com.bnta.airline.repositories.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class FlightService {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    public List<Flight> findAllFlightsInfo(){
+        return flightRepository.findAll();
+    }
+
+    public Flight findFlightInfo(Long id){
+        return flightRepository.findById(id).get();
+    }
+
+    public void saveFlight(Flight flight){
+        flightRepository.save(flight);
+    }
+
+    public void cancelFlight(Long id){
+        flightRepository.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/bnta/airline/services/PassengerService.java
+++ src/main/java/com/bnta/airline/services/PassengerService.java
@@ -1,9 +1,14 @@
 package com.bnta.airline.services;
 
+import com.bnta.airline.models.Flight;
+import com.bnta.airline.models.Passenger;
+import com.bnta.airline.models.PassengerDTO;
 import com.bnta.airline.repositories.FlightRepository;
 import com.bnta.airline.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -13,5 +18,27 @@ public class PassengerService {
 
     @Autowired
     FlightRepository flightRepository;
+
+    public void bookPassenger(PassengerDTO passengerDTO){
+        Passenger passenger = new Passenger(passengerDTO.getName(), passengerDTO.getEmailAddress());
+
+        for (Long flightId : passengerDTO.getFlightIds()){
+            Flight flight = flightRepository.findById(flightId).get();
+            passenger.addFlight(flight);
+        }
+        passengerRepository.save(passenger);
+    }
+
+    public Passenger displayPassengerInfo(Long id){
+        return passengerRepository.findById(id).get();
+    }
+
+    public List<Passenger> displayAllPassengersInfo() {
+        return passengerRepository.findAll();
+    }
+
+    public void addPassenger(Passenger passenger){
+        passengerRepository.save(passenger);
+    }
 
 }
