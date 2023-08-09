291caacbcf413180ff2f7ea91ed621d8de60035c
mikeycb96 <michaelsonbaptist@gmail.com>
2023-08-09 17:03:53 +0100

    completed POJOs

--- src/main/java/com/bnta/airline/models/Passenger.java
+++ src/main/java/com/bnta/airline/models/Passenger.java
@@ -1,4 +1,85 @@
 package com.bnta.airline.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "passengers")
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String emailAddress;
+
+    @JsonIgnoreProperties({"passengers"})
+    @ManyToMany
+    @JoinTable(
+            name = "passengers_flights",
+            joinColumns = @JoinColumn(name = "passenger_id"),
+            inverseJoinColumns = @JoinColumn(name = "flight_id")
+    )
+
+    private List<Flight> flights;
+
+    public Passenger(String name, String emailAddress){
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.flights = new ArrayList<>();
+    }
+     public Passenger(){
+
+     }
+
+    public void addFlight(Flight flight){
+        this.flights.add(flight);
+    }
+
+    public void removeFlight(Flight flight){
+        this.flights.remove(flight);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
 }
+
+
+
+
